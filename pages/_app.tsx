42d9054ab8bf8e2f3520f9c4b408705d2a750002
Kamran Ansari <ranmerc@outlook.com>
2023-08-25 13:45:34 +0530

    feat: add emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,42 @@
+// https://github.com/mui/material-ui/blob/master/examples/material-ui-nextjs-pages-router-ts/pages/_app.tsx
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/styles/theme";
 import Header from "@/components/Header";
+import { QueryClient, QueryClientProvider } from "react-query";
+import CartDrawer from "@/components/CartDrawer";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "@/utils/createEmotionCache";
 
-export default function App({ Component, pageProps }: AppProps) {
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const queryClient = new QueryClient();
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Header />
-        <Component {...pageProps} />;
-      </ThemeProvider>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Header />
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+            {/* <CartDrawer /> */}
+          </QueryClientProvider>
+        </ThemeProvider>
+      </CacheProvider>
     </>
   );
 }
